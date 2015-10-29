3ddeddb75aa02aa2204b54baac675b0d91ff7a39
Michal Špaček <tupinek@gmail.com>
2015-10-29 14:25:16 +0100

    Prepare for 0.04 version.

--- TGF.pm
+++ TGF.pm
@@ -10,7 +10,7 @@ use Encode qw(decode_utf8);
 use Error::Pure qw(err);
 
 # Version.
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Edge callback.
 sub _edge_callback {
@@ -277,6 +277,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
